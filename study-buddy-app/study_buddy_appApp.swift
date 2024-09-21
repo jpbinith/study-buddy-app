b2c67781e82ad6a331689eddde19396cc83cb443
jpbinith <jpbinith@gmail.com>
2024-09-21 15:23:04 +1000

    Initial Commit

---
+++ study-buddy-app/study_buddy_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  study_buddy_appApp.swift
+//  study-buddy-app
+//
+//  Created by Binith Jayasinghe on 21/9/2024.
+//
+
+import SwiftUI
+
+@main
+struct study_buddy_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
